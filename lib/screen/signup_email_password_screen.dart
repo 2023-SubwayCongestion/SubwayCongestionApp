cd7530c2f61c4082e4c29055fd0d6625d294b978
Hyunwoo Kang <gusdn741021@gmail.com>
2023-06-26 19:38:18 +0900

    회원가입, 로그인 화면 디자인 적용

--- lib/screen/signup_email_password_screen.dart
+++ lib/screen/signup_email_password_screen.dart
@@ -1,4 +1,5 @@
 import 'package:subway_congestion/services/firebase_auth_methods.dart';
+import 'package:subway_congestion/widget/custom_image.dart';
 import 'package:subway_congestion/widget/custom_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -26,46 +27,57 @@ class _EmailPasswordSignupState extends State<EmailPasswordSignup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          const Text(
-            "회원가입",
-            style: TextStyle(fontSize: 30),
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.08),
           Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: CustomTextField(
-              controller: emailController,
-              hintText: '이메일을 입력해주세요.',
-            ),
+            width: double.maxFinite,
+            height: double.maxFinite,
+            color: Color.fromRGBO(245, 243, 228, 1),
           ),
-          const SizedBox(height: 20),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: CustomTextField(
-              controller: passwordController,
-              hintText: '비밀번호를 입력해주세요.',
+          Center(
+            child: Column(
+              children: [
+                CustomImage(
+                  filename: 'assets/sign up.png',
+                  widthPercent: 0.5,
+                  heightPercent: 0.5,
+                ),
+                // SizedBox(height: MediaQuery.of(context).size.height * 0.005),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: CustomTextField(
+                    controller: emailController,
+                    hintText: '이메일을 입력해주세요.',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: CustomTextField(
+                    controller: passwordController,
+                    hintText: '비밀번호를 입력해주세요.',
+                  ),
+                ),
+                const SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: signUpUser,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Color.fromRGBO(84, 162, 154, 1)),
+                    textStyle: MaterialStateProperty.all(
+                      const TextStyle(color: Colors.white),
+                    ),
+                    minimumSize: MaterialStateProperty.all(
+                      Size(MediaQuery.of(context).size.width / 2.5, 50),
+                    ),
+                  ),
+                  child: const Text(
+                    "회원가입하기",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ],
             ),
-          ),
-          const SizedBox(height: 40),
-          ElevatedButton(
-            onPressed: signUpUser,
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.blue),
-              textStyle: MaterialStateProperty.all(
-                const TextStyle(color: Colors.white),
-              ),
-              minimumSize: MaterialStateProperty.all(
-                Size(MediaQuery.of(context).size.width / 2.5, 50),
-              ),
-            ),
-            child: const Text(
-              "회원가입하기",
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-          ),
+          )
         ],
       ),
     );
--- lib/widget/custom_textfield.dart
+++ lib/widget/custom_textfield.dart
@@ -12,25 +12,46 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Colors.transparent, width: 0),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Colors.transparent, width: 0),
-        ),
-        contentPadding:
-        const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-        filled: true,
-        fillColor: const Color(0xffF5F6FA),
-        hintText: hintText,
-        hintStyle: const TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.w400,
+    // 화면 크기 구하기
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    // 버튼 크기를 화면 크기의 일정 비율로 설정하기
+    final textFieldWidth = screenWidth * 0.5; // 화면 폭의 90%로 버튼 폭 설정
+    final textFieldHeight = screenHeight * 0.05; // 화면 높이의 10%로 버튼 높이 설정
+    return Container(
+      width: textFieldWidth,
+      height: textFieldHeight,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5), // 그림자 색상
+            spreadRadius: 1, // 그림자 확산 정도
+            blurRadius: 6, // 그림자 흐림 정도
+            offset: Offset(0, 3), // 그림자 위치
+          ),
+        ],
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(color: Colors.transparent, width: 0),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(color: Colors.transparent, width: 0),
+          ),
+          contentPadding:
+          const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+          filled: true,
+          fillColor: const Color(0xffF5F6FA),
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),
         ),
       ),
     );
